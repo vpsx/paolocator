c013256743d658a895a6c4347469d1a06cd110be
vpsx <19900057+vpsx@users.noreply.github.com>
2021-11-04 15:48:23 +0000

    Fix magnetometer issues: Update Blinka/LSM303 library

--- magnetotest.py
+++ magnetotest.py
@@ -2,11 +2,14 @@ import time
 import math
 import board
 import busio
-import adafruit_lsm303
+import adafruit_lsm303_accel
+import adafruit_lsm303dlh_mag
 
 # Yay accelerometer and magnetometer!
 i2c = busio.I2C(board.SCL, board.SDA)
-accelmag = adafruit_lsm303.LSM303(i2c)
+accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
+mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
+
 
 last_acm_print = time.monotonic()
 heading = 0
@@ -15,11 +18,11 @@ while True:
     # Every second print out current accel/mag readings.
     if current - last_acm_print >= 1.0:
         last_acm_print = current
-        acc_x, acc_y, acc_z = accelmag.acceleration
-        mag_x, mag_y, mag_z = accelmag.magnetic
+        acc_x, acc_y, acc_z = accel.acceleration   # m/s^2
+        mag_x, mag_y, mag_z = mag.magnetic         # micro-Teslas
 
         #print('Acceleration (m/s^2): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(acc_x, acc_y, acc_z))
-        #print('Magnetometer (gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(mag_x, mag_y, mag_z))
+        #print('Magnetometer (mcr-T): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(mag_x, mag_y, mag_z))
 
         acc_norm = math.sqrt(acc_x * acc_x + acc_y * acc_y + acc_z * acc_z)
         pitch = math.asin(acc_x/acc_norm)
